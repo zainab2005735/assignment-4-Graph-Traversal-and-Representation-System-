105cd83e3b4770731a59edc73359578f8d3c0618
zainab2005735 <noorizainab221@gmail.com>
2026-05-23 11:18:11 +0500

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
-
 public class Main {
     public static void main(String[] args) {
-        Experiment experiment = new Experiment();
+
+        Experiment experiment =
+                new Experiment();
+
         experiment.runMultipleTests();
         experiment.printResults();
     }
